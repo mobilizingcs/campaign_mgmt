967dccb03c19b3ab864f0046592c8725054094dd
Jeroen Ooms <jeroenooms@gmail.com>
2014-10-29 16:47:20 -0700

    updates

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 (function(){
     //initiate the client
-    var oh = Ohmage("/app", "campaign-author-tool-reloaded")
+    var oh = Ohmage("/app", "campaign-manager")
 
     //attach global callbacks
     oh.callback("done", function(x, status, req){
@@ -10,7 +10,7 @@
 
     //global error handler. In ohmage 200 means unauthenticated
     oh.callback("error", function(msg, code, req){
-        (code == 200) ? window.location.replace("../web/#login") : alert("Error!\n" + msg);
+        (code == 200) ? window.location.replace("../web/#login") : message("<strong>Error! </strong>" + msg);
     });
 
     //prevent timeout
@@ -29,6 +29,8 @@
                 var roles = data[urn]["user_roles"];
                 if($.inArray("author", roles) > -1 || $.inArray("supervisor", roles) > -1) {
 
+                    var count = -1;
+
                     var tr = $("<tr>").appendTo("#campaigntablebody")
                     var td1 = $("<td>").appendTo(tr).text(data[urn].name);
                     var td2 = $("<td>").appendTo(tr).text(data[urn].creation_timestamp);
@@ -36,63 +38,64 @@
                     var td4 = $("<td>").appendTo(tr);
                     var td5 = $("<td>").appendTo(tr);
 
-                    var a = $("<a>").addClass("btn disabled").text("Update").appendTo(td5).click(function(e) {
-                        var self = $(this);
+                    var btn = $("<div />").addClass("btn-group").append('\
+                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"> \
+                        <span class="glyphicon glyphicon glyphicon-folder-open"></span></button>').appendTo(td5);
+
+                    var ul = $("<ul />").addClass("dropdown-menu").attr("role", "menu").appendTo(btn);
+                    var a1 = $("<a />").appendTo($("<li />").appendTo(ul)).append('<span class="glyphicon glyphicon-th-list"></span> Edit Surveys').attr("href", "#").click(function(e){
                         e.preventDefault();
-                        if(self.hasClass("disabled")) return;
+                        if(count < 0){
+                            message("Loading campaign info, please be patient.", "info")
+                        } else if(count > 0){
+                            message("Campaign <strong>" + urn + "</strong> has existing responses and can therefore not be modified.")
+                        } else {
+                            message("Placeholder for updating campaign.", "info")
+                        }
+                    });
 
+                    var a2 = $("<a />").appendTo($("<li />").appendTo(ul)).append('<span class="glyphicon glyphicon-cog"></span> Change State').attr("href", "#").click(function(e){
+                        message("Placeholder for update status", "success")
+                    });
+
+                    var a3 = $("<a />").appendTo($("<li />").appendTo(ul)).append('<span class="glyphicon glyphicon-picture"></span> Visualize').attr("href", "#").click(function(e){
+                        e.preventDefault();
+                        if(count < 0){
+                            message("Loading campaign info, please be patient.", "info")
+                        } else if(count === 0){
+                            message("Campaign <b>" + urn + "</b> has no existing responses. Nothing to visualize.")
+                        } else {
+                            window.location.href = '../dashboard/#' + urn;
+                        }
+                    });
+
+                    var a4 = $("<a />").appendTo($("<li />").appendTo(ul)).append('<span class="glyphicon glyphicon-floppy-save"></span> Download XML').attr("href", "#").click(function(e){
+                        e.preventDefault();
+                        var popup = window.open()
                         oh.campaign.readall({
-                            campaign_urn_list: urn,
-                            output_format:"long"
+                            campaign_urn_list : urn,
+                            output_format : "xml"
                         }).done(function(data){
-                            var campaign = data[urn];
-                            try {
-                                var json = campaigntojson(campaign.xml.replace(/<!--[\s\S]*?-->/g, ""))
-                            } catch(err){
-                                alert("Error parsing XML: \n\n" + err.message)
-                            }
-
-                            var surveys = [];
-                            if(json.campaign && json.campaign.surveys){
-                                surveys = json.campaign.surveys;
-                            } else {
-                                alert("No surveys found in XML. Perhaps invalid XML.")
-                            }
-
-                            localStorage['campaignWrapper'] = JSON.stringify({
-                                classes : campaign.classes[0],
-                                oldclasses : campaign.classes[0],
-                                description : campaign.description,
-                                privacyState : campaign.privacy_state,
-                                runningState : campaign.running_state,
-                                username : username,
-                                update : true,
-                                campaign : {
-                                    campaignName: campaign.name,
-                                    campaignUrn : urn,
-                                    surveys : surveys
-                                }
-                            });
-
-                            //ftw
-                            $.cookie('currentSurvey', 0)
-                            window.location.href = self.attr('href');
+                            //doesn't work properly, not sure why
+                            popup.document.write(data)
                         });
                     });
+
+                    var a5 = $("<a />").appendTo($("<li />").appendTo(ul)).append('<span class="glyphicon glyphicon-file"></span> Export Data')
+                        .attr("href", "../../app/survey_response/read?campaign_urn=" + urn + "&privacy_state=shared&client=manager&user_list=urn:ohmage:special:all&prompt_id_list=urn:ohmage:special:all&output_format=csv&sort_oder=timestamp&column_list=urn:ohmage:user:id,urn:ohmage:context:timestamp,urn:ohmage:prompt:response,urn:ohmage:context:location:latitude,urn:ohmage:context:location:longitude&suppress_metadata=true")
 
                     return oh.survey.count(urn).done(function(counts){
                         if(!Object.keys(counts).length){
                             //no existing responses found
-                            a.removeClass("disabled").addClass("btn-primary").attr("href", "campaign-edit.html");
-                            td4.text(0);
+                            count = 0;
                         } else {
-                            var total = $.map(counts, function(val, key) {
+                            count = $.map(counts, function(val, key) {
                                 return val[0].count;
                             }).reduce(function(previousValue, currentValue) {
                                 return previousValue + currentValue;
                             });
-                            td4.text(total);
                         }
+                        td4.text(count);
                     });
                 }
             });
@@ -117,4 +120,10 @@
             });
         });
     });
+
+    function message(msg, type){
+        // type must be one of success, info, warning, danger
+        type = type || "danger"
+        $("#errordiv").empty().append('<div class="alert alert-' + type + '"><a href="#" class="close" data-dismiss="alert">&times;</a>' + msg + '</div>');
+    }
 })();
